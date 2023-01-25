85d4e0ef0413aa329f882ca27bd86de80d228989
oip <oip@anadeainc.com>
2023-01-25 18:26:45 +0200

    prettier

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common'
-import { JwtModule } from '@nestjs/jwt'
-import { AuthService } from './auth.service'
-import { AuthController } from './auth.controller'
-import {ConfigModule} from "@nestjs/config"
-import {FirebaseService} from "../../firebase/firebase.service"
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { ConfigModule } from '@nestjs/config';
+import { FirebaseService } from '../../firebase/firebase.service';
 
 @Module({
   imports: [JwtModule.register({}), ConfigModule],
