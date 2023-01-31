5ffa13ecd56388c14ee3904824a9fa16d56e2f4f
OIPonomarenko <92428159+OIPonomarenko@users.noreply.github.com>
2023-01-31 17:04:25 +0200

    add configuration

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create<INestApplication>(AppModule);
+  await app.listen(process.env.PORT);
+}
+bootstrap();
---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  public signup(@Body() dto: AuthDto) {
+    return this.authService.signup(dto);
+  }
+
+  @Post('login')
+  public login(@Body() dto: AuthDto) {
+    return this.authService.login(dto);
+  }
+}
