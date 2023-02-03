10c15113dc5e79f30fb1ecca60246c9b6408de06
oip <oip@anadeainc.com>
2023-02-03 16:41:36 +0200

    add test find one

--- src/modules/tricks/tricks.controller.spec.ts
+++ src/modules/tricks/tricks.controller.spec.ts
@@ -1,7 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TricksController } from './tricks.controller';
 import { TricksService } from './tricks.service';
-import { FirebaseService } from '../../firebase/firebase.service'
+import { FirebaseService } from '../../firebase/firebase.service';
+import { CreateTrickDto } from '../tricks/dto/create-trick.dto';
 
 describe('TricksController', () => {
   let controller: TricksController;
@@ -16,14 +17,27 @@ describe('TricksController', () => {
   describe('findAll', () => {
     it('should return an array of tricks', async () => {
       const result = ['test'];
-      jest.spyOn(tricksService, 'findAll')
-          .mockImplementation(() => {
-            return new Promise((resolve): void => {
-              resolve(result);
-            });
-          });
+      jest.spyOn(tricksService, 'findAll').mockImplementation(() => {
+        return new Promise((resolve): void => {
+          resolve(result);
+        });
+      });
 
       expect(await controller.findAll()).toBe(result);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return trick', async () => {
+      const fakeId = '123'
+      const result = new CreateTrickDto();
+      jest.spyOn(tricksService, 'findOne').mockImplementation((fakeId) => {
+        return new Promise((resolve): void => {
+          resolve(result);
+        });
+      });
+
+      expect(await controller.findOne(fakeId)).toBe(result);
     });
   });
 
