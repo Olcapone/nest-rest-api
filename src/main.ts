9f64aaef67efeedb61c02141daf1f6757415ad2f
OIPonomarenko <92428159+OIPonomarenko@users.noreply.github.com>
2023-02-03 17:37:39 +0200

    add firebase config to auth middle bar (#7)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 import { INestApplication } from '@nestjs/common';
+import { initializeApp, getApps, cert, ServiceAccount } from "firebase-admin/app";
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<INestApplication>(AppModule);
+  const configService: ConfigService = app.get(ConfigService);
+  const adminConfig: ServiceAccount = {
+    "projectId": configService.get<string>('projectId'),
+    "privateKey": configService.get<string>('FB_PRIVATE_KEY')
+        .replace(/\\n/g, '\n'),
+    "clientEmail": configService.get<string>('FB_CLIENT_EMAIL'),
+  };
+
+  initializeApp({
+    credential: cert(adminConfig),
+  });
+  
   await app.listen(process.env.PORT);
 }
 bootstrap();
