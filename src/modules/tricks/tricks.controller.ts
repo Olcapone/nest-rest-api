70a1a460087ea71a29aaf0897c8adb9080f65f3b
oip <oip@anadeainc.com>
2023-02-03 15:39:40 +0200

    add services, controllers and tests

--- src/modules/tricks/tricks.controller.ts
+++ src/modules/tricks/tricks.controller.ts
@@ -4,6 +4,7 @@ import {
   Post,
   Body,
   Patch,
+  Put,
   Param,
   Delete,
   Req
@@ -11,7 +12,7 @@ import {
 import { Request } from 'express';
 import { TricksService } from './tricks.service';
 import { CreateTrickDto } from './dto/create-trick.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { UpdateTrickDto } from './dto/update-trick.dto';
 
 @Controller('tricks')
 export class TricksController {
@@ -23,22 +24,22 @@ export class TricksController {
   }
 
   @Get()
-  findAll(@Req() request: Request) {
+  findAll() {
     return this.tricksService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.tricksService.findOne(+id);
+    return this.tricksService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.tricksService.update(+id, updateUserDto);
+  update(@Param('id') id: string, @Body() updateTrickDto: UpdateTrickDto) {
+    return this.tricksService.update(id, updateTrickDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.tricksService.remove(+id);
+    return this.tricksService.remove(id);
   }
 }
